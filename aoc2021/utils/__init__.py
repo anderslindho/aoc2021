840f50a8485aed90d889b7ab14cbeb104f2fc6b0
ades <anders.lindholsson@gmail.com>
2021-12-03 22:47:34 +0100

    Add support for advent-of-code-data

--- aoc2021/utils/__init__.py
+++ aoc2021/utils/__init__.py
@@ -5,7 +5,9 @@ PACKAGE_DIR = pathlib.Path(__file__).parent.parent
 
 def read_data(path: str) -> list:
     with open(path, "r") as f:
-        return f.read().split("\n")
+        # advent-of-code-data will download files that end with a newline
+        # so we have to make sure that there aren't any empty strings
+        return [line for line in f.read().split("\n") if line]
 
 
 def convert_strbits_to_dec(bits: str) -> int:
