813c31060dd75408ae56900def61f53551a527df
ades <anders.lindholsson@gmail.com>
2021-12-04 03:10:52 +0100

    Test on python3.8

--- aoc2021/__init__.py
+++ aoc2021/__init__.py
@@ -1,3 +1,5 @@
+import sys
+
 import aoc2021.day1
 import aoc2021.day2
 import aoc2021.day3
@@ -14,4 +16,5 @@ def solve(day: int, part: int, data: list) -> int:
     elif day == 4:
         return aoc2021.day4.solve(part, data)
     else:
-        raise NotImplementedError
+        print("Not a valid choice.")
+        sys.exit(-1)
--- aoc2021/__main__.py
+++ aoc2021/__main__.py
@@ -1,6 +1,6 @@
 from argparse import ArgumentParser
 
-import aoc2021
+from aoc2021 import solve
 from aoc2021.utils import PACKAGE_DIR, read_data
 
 
@@ -9,7 +9,7 @@ def main(day: int):
     print(f"Day {day}")
     day = int(day)
     for part in (1, 2):
-        print(f"The solution to part {part} is: {aoc2021.solve(day, part, data)}")
+        print(f"The solution to part {part} is: {solve(day, part, data)}")
 
 
 parser = ArgumentParser()
